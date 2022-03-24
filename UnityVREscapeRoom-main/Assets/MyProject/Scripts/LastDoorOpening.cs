09f44035a730cd63c4851dfac7a97b8caa690a21
Pablo Cano San Román <c015653k@student.staffs.ac.uk>
2022-03-24 21:16:50 +0000

    Game made (Still have to implement sound and UI and do the documentation)

--- UnityVREscapeRoom-main/Assets/MyProject/Scripts/LastDoorOpening.cs
+++ UnityVREscapeRoom-main/Assets/MyProject/Scripts/LastDoorOpening.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class LastDoorOpening : MonoBehaviour
 {
-    [SerializeField] private GameObject m_RightDoor;
-    [SerializeField] private GameObject m_LeftDoor;
+    [SerializeField] private Animator m_LastDoor;
     [SerializeField] private float m_PositionChangeSpeed;
 
     public void OpeningDoors()
     {
-        Vector3 leftDoorEndPos = new Vector3(12.89f, 2.33f, 4.502f);
-        Vector3 rightDoorEndPos = new Vector3(12.89f, 2.33f, -4.525f);
-
-        m_LeftDoor.transform.position = Vector3.Lerp(m_LeftDoor.transform.position, leftDoorEndPos, m_PositionChangeSpeed * Time.deltaTime);
-        m_RightDoor.transform.position = Vector3.Lerp(m_RightDoor.transform.position, rightDoorEndPos, m_PositionChangeSpeed * Time.deltaTime);
+        m_LastDoor.SetTrigger("OpeningLastDoor");
     }
 }
